f8961991b52d07628d09461cd463f6594f3185fb
Logan Whitty <lowhitty7@gmail.com>
2025-01-28 22:38:41 -0500

    Adding the completed assignment of the lesson

---
+++ Javascript/Variables_And_Operators/Assignment.js
@@ -0,0 +1,18 @@
+console.log(23 + 97)
+console.log((4 + 6 + 9) / 77)
+
+let a = 10;
+console.log(a);
+
+a = 7;
+console.log(a);
+
+let b = 9 * a;
+console.log(b);
+
+
+const max = 57;
+const actual = max - 13;
+const percentage = actual/max;
+
+console.log(percentage)
