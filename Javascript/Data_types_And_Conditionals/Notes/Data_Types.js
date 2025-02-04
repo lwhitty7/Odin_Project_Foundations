ef48e67919ab0bd42de99ca9aa28f735f18d213e
Logan Whitty <lowhitty7@gmail.com>
2025-02-03 23:09:25 -0500

    adding short notes on two data types

---
+++ Javascript/Data_types_And_Conditionals/Notes/Data_Types.js
@@ -0,0 +1,14 @@
+//Number 
+let n = 123;
+n = 12.345;
+
+alert( 1 / 0 ); // Infinity
+alert( Infinity ); // Infinity
+
+
+// Bigint
+// In JavaScript, the “number” type cannot safely represent integer values
+//  larger than (253-1) (that’s 9007199254740991), or less than -(253-1) for negatives.
+
+console.log(9007199254740991 + 1); // 9007199254740992
+console.log(9007199254740991 + 2); // 9007199254740992
