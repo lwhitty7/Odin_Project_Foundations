6010d274bb9774cd119fd4c837af5ac93aefac3b
Logan Whitty <lowhitty7@gmail.com>
2025-02-27 21:06:34 -0500

    Getting rid of notes

--- Javascript/Variables_And_Operators/hello.js
+++ Javascript/Variables_And_Operators/hello.js
@@ -6,9 +6,3 @@ let y = "hello";
 let a = x + y;
 let b = y+x;
 
-// what even is javascript lmao ill need to reread the documentation
-// DO WORK TOMMOROW YOU SICK BUM
-// oh!
-// lock in
-// did not lock in burh
-// tommorow is the day i almost complete the unit
