75516ec63e0beb0be01418cb0a7f43be9de68c54
Logan Whitty <lowhitty7@gmail.com>
2025-03-02 21:58:17 -0500

    changing notes from online to paper notes

--- Javascript/Data_types_And_Conditionals/w3schools_JS_string_methods/strings.js
+++ Javascript/Data_types_And_Conditionals/w3schools_JS_string_methods/strings.js
@@ -4,4 +4,12 @@ let lenght = rah.length;
 
 
 // While for extracting characters at a string there are a four methods
-// starting tommorow! i mean tomorow's tomorows
+// These include
+// at(position
+// charAt(position)
+// charCodeAt(poisiton)
+
+let boom = "Hello";
+let char = boom.charAt(0) //This will return H since H is at position 0 in string boom
+
+
